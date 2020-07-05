d2e2176b74062c6a27875cb2d7c9e995c7c2270d
abechoi <abraham.choi@icloud.com>
2020-07-04 19:22:21 -0700

    Intro to Hooks

---
+++ src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SongList = () => {
+  return (
+    <div>
+      <ul>
+        <li>This Wild Darkness</li>
+        <li>Memory Gospel</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SongList;
